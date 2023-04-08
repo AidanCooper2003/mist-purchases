7f690d1da3ba4de1da67e2cf5b2368e8740a7bcf
Aidan Cooper <aidcoope@iu.edu>
2023-04-08 16:29:06 -0400

    Finished Skeleton

---
+++ src/main/java/edu/iu/c322/mist/purchases/controller/HomeController.java
@@ -0,0 +1,4 @@
+package edu.iu.c322.mist.purchases.controller;
+
+public class HomeController {
+}
---
+++ src/main/java/edu/iu/c322/mist/purchases/repository/UserListRepository.java
@@ -0,0 +1,4 @@
+package edu.iu.c322.mist.purchases.repository;
+
+public interface UserListRepository {
+}
