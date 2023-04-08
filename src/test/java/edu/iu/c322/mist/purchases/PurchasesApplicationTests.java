074c551e9a69f10633ae499c06e0360c779670bd
Aidan Cooper <aidcoope@iu.edu>
2023-04-08 14:32:09 -0400

    Spring Intializer Initial Commit

---
+++ src/test/java/edu/iu/c322/mist/purchases/PurchasesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.mist.purchases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
